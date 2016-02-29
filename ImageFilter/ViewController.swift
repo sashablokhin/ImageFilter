27532c74a869da2c7bf638a0e39924a1d6526e3f
sashablokhin <sashablokhindev@gmail.com>
2016-02-29 17:47:22 +0300

    Try apply image filter

--- ImageFilter/ViewController.swift
+++ ImageFilter/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let image = imageView?.image, cgimg = image.CGImage else {
+            print("imageView doesn't have an image!")
+            return
+        }
+        
+        let coreImage = CIImage(CGImage: cgimg)
+        
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(coreImage, forKey: kCIInputImageKey)
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        
+        if let output = filter?.valueForKey(kCIOutputImageKey) as? CIImage {
+            let filteredImage = UIImage(CIImage: output)
+            imageView?.image = filteredImage
+        }
+            
+        else {
+            print("image filtering failed")
+        }
     }
 
     override func didReceiveMemoryWarning() {
